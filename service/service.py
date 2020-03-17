d450c41c12b7213346da0d48314808c21fe5a8cc
Andrey Zelenovsky <azelenovsky@gmail.com>
2020-03-16 20:06:31 -0400

    Functionality to Update Existing Inventory Item

--- service/service.py
+++ service/service.py
@@ -26,6 +26,26 @@ def index():
     """ Root URL response """
     return "Reminder: return some useful information in json format about the service here", status.HTTP_200_OK
 
+######################################################################
+# UPDATE AN EXISTING INVENTORY ITEM
+######################################################################
+@app.route("/")
+def update_inventory():
+    """
+    Update an inventory item
+
+    This endpoint will update an inventory item based on the body that is posted
+    """
+    app.logger.info("Request to update inventory with sku: %s", sku)
+    check_content_type("application/json")
+    inventory = Inventory.find_by_sku(sku)
+    if not inventory:
+        raise NotFound("Inventory with sku '{}' was not found.".format(sku))
+    inventory.deserialize(request.get_json())
+    inventory.sku = sku 
+    inventory.save()
+    return make_response(jsonify(inventory.serialize()), status.HTTP_200_OK)
+
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
