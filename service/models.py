70b2cf99130244f37b078e8af6727be57681bade
Andrey Zelenovsky <azelenovsky@gmail.com>
2020-04-09 13:15:48 -0400

    Testing travis

--- service/models.py
+++ service/models.py
@@ -9,6 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 logger = logging.getLogger("flask.app")
 
 # Create the SQLAlchemy object to be initialized later in init_db()
+#
 db = SQLAlchemy()
 
 class DataValidationError(Exception):
