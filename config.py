4d80dd96829137b73841820cd27e4ec5c6e2cf76
Andrey Zelenovsky <azelenovsky@gmail.com>
2020-04-11 22:47:32 -0400

    updated config.py to check for VCAPs

--- config.py
+++ config.py
@@ -2,9 +2,15 @@
 Global Configuration for Application
 """
 import os
+import json
 
 # Get configuration from environment
 DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///../development.db")
+
+#override for cloud foundry
+if 'VCAP_SERVICES' in os.environ:
+    vcap = json.loads(os.environ['VCAP_SERVICES'])
+    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
